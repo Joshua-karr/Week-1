3fc0247be5f8080c33cbbfa708a7279264f6fef7
Joshua-karr <64676125+Joshua-karr@users.noreply.github.com>
2020-05-10 11:35:25 -0700

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,8 @@
+
+public class Introduction {
+	public static void main(String[] args) {
+		System.out.println("My name is Joshua Karr.");
+		System.out.println("My favorite color is green.");
+		System.out.println("If I won the lottery, I would buy my dad a house on a lake and invest the rest.");
+	}
+}
